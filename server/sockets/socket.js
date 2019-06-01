cf9f4697c9f3f773bd7566c602b8ce682ea71b9f
Oscar Gonzalez <osmaogonzalez@gmail.com>
2019-06-01 13:02:20 -0500

    Primera versión del chat con sockets

---
+++ server/sockets/socket.js
@@ -0,0 +1,50 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades')
+
+const usuarios = new Usuarios();
+
+io.on('connection', (client) => {
+
+    client.on('entrarChat', (data, callback) => {
+        console.log(data);
+
+        if (!data.nombre || !data.sala) {
+            return callback({
+                err: true,
+                mensaje: 'nombre/sala necesario'
+            })
+        }
+
+        client.join(data.sala);
+
+        let personas = usuarios.agregarPersona(client.id, data.nombre, data.sala)
+
+        client.broadcast.to(data.sala).emit('usuarioConectado', usuarios.getPersonasPorSala(data.sala));
+
+        callback(personas);
+    });
+
+    client.on('disconnect', () => {
+        let personaDesconectada = usuarios.borrarPersona(client.id);
+
+        client.broadcast.to(personaDesconectada.sala).emit('usuarioDesconectado', crearMensaje('Administrador', `${personaDesconectada.nombre} ha salido del chat`))
+        client.broadcast.to(personaDesconectada.sala).emit('usuarioDesconectado', usuarios.getPersonasPorSala(personaDesconectada.sala));
+    })
+
+    client.on('enviarMensaje', (data) => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.to(persona.sala).emit('recibirMensaje', mensaje);
+    });
+
+    //mensajes privados
+    client.on('mensajePrivado', data => {
+        let persona = usuarios.getPersona(client.id);
+
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+    });
+
+});
